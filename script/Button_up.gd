d77539e02b3e29fd82f2097961b9f813a3f6679a
azhar81 <a.hassanuddin02@gmail.com>
2021-11-15 20:05:49 +0700

    temporary map

--- script/Button_up.gd
+++ script/Button_up.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 
 onready var button_down = load("res://scenes/Button_down.tscn")
+onready var crate_small = load("res://scenes/Crate_small.tscn")
+onready var crate_big = load("res://scenes/Crate_big.tscn")
 
 func _on_Area2D_body_entered(body):
 	if body is Slime:
@@ -8,4 +10,12 @@ func _on_Area2D_body_entered(body):
 		var instance = button_down.instance()
 		var pos = get_position()
 		instance.set_position(pos)
-		get_parent().add_child(instance)
+		get_parent().call_deferred("add_child", instance)
+		
+		instance = crate_small.instance()
+		instance.set_position(Vector2(386.878, 287.868))
+		get_parent().call_deferred("add_child", instance)
+	
+		instance = crate_big.instance()
+		instance.set_position(Vector2(467.191, 280.017))
+		get_parent().call_deferred("add_child", instance)
